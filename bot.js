818724262feba3620ffdd875b7d33c55a1b18f6a
krebinski j <krebindopagode@gmail.com>
2025-02-16 16:09:33 +0000

    fix: fazendo rework do bot

--- bot.js
+++ bot.js
@@ -4,7 +4,11 @@ const bot = mineflayer.createBot({
     host: 'krebinkkj.aternos.me', // Endereço de ip do servidor
     port: 17968, // Porta do servidor 
     username: 'IA-VAZIO',// Nickname do bot
+<<<<<<< HEAD
     version: false // Versão do Minecraft (1.20/1.19). O padrão é 'false' (auto-detectar)
+=======
+    version: false // Versão do Minecraft (Ex: 1.20/1.19). O padrão é 'false' (auto-detectar)
+>>>>>>> 770d811 (fix: fazendo rework do bot)
 })
 
 bot.on('spawn', () => {
